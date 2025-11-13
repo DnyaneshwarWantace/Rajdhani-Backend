93f28963521271c03cad17765646b4166bb64063
DnyaneshwarWantace <dnyaneshwar@wantace.com>
2025-11-13 17:27:36 +0530

    calender changes

--- clear-via-api.js
+++ clear-via-api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://rajdhani.wantace.com/api';
 
 async function fetchJson(url, options = {}) {
   const response = await fetch(url, {
